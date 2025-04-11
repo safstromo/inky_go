644cdf94c6475ec18b45bee79e352ce345ec95e1
Oliver Säfström <oliver.safstrom@mobilityguard.com>
2025-04-11 11:29:18 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	log.Println("listening on port :3000")
+	http.ListenAndServe(":3000", r)
+}
